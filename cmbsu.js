e737a7353074b721167a56b8bffcef467c4e304a
romsys <romsys@172.16.10.79>
2015-04-10 13:26:28 +0300

    sdfsd

--- cmbsu.js
+++ cmbsu.js
@@ -1,2 +1,24 @@
-alert('ana');
-alert(document.getElementById('T:wc_psl36::r'));
+var CMBSU = {
+
+	loadScript: function(url, callback) {
+		    // Adding the script tag to the head as suggested before
+		    var head = document.getElementsByTagName('head')[0];
+		    var script = document.createElement('script');
+		    script.type = 'text/javascript';
+		    script.src = url;
+
+		    // Then bind the event to the callback function.
+		    // There are several events for cross browser compatibility.
+		    script.onreadystatechange = callback;
+		    script.onload = callback;
+
+		    // Fire the loading
+		    head.appendChild(script);
+	},
+
+    init: function() {
+    	CMBSU.loadScript('http://yui.yahooapis.com/3.18.1/build/yui/yui-min.js', function(){});
+    }
+};
+
+CMBSU.init();
