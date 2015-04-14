15e91603b0192b249fec5332b538d698bd998853
romsys <romsys@172.16.10.79>
2015-04-14 15:58:48 +0300

    asda

--- cmbsu_css.js
+++ cmbsu_css.js
@@ -3,8 +3,7 @@ var CMBSU_CSS = {
 	_Y: null,
 	
 	createPageDropEditor: function() {
-		//alert(_Y.one(document'#T:wc_psl36::r'));
-		//alert(_Y.one('T:wc_psl36::r'));
+
 		_Y.one(document.getElementById('T:wc_psl36::r')).setStyle('right', '36px');
 		
 		_Y.one(document.getElementById('T:globNavItms')).setStyle('position', 'relative');
