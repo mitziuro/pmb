188b6f7291c1a26e5f0bbec36f9990068bf6b2d0
romsys <romsys@172.16.10.79>
2015-04-14 16:14:40 +0300

    zdfszfd

--- cmbsu_css.js
+++ cmbsu_css.js
@@ -10,14 +10,41 @@ var CMBSU_CSS = {
 		_Y.one(document.getElementById('T:globNavItms')).setStyle('left', '-1px');
 		
 	},
+	
+	createSliderSearch: function() {
+
+		var node = _Y.one(document.getElementById('T:searchbox:srchFrm:siSrTx::content'));
+		
+		node.on('click', new function(e) {
+			
+			var anim = new _Y.Anim({
+		        node: node,
+		        from: {
+		            width: '170px'
+		        },
+
+		        to: {
+		        	 width: '500px'
+		        },
+
+		        duration: 1,
+		        iterations: 1
+		    });
+
+			anim.run();
+		});
+
+		
+	},
 
     init: function() {
     	
-    	YUI().use('node', function(Y) {
+    	YUI().use('node', 'anim', function(Y) {
     		_Y = Y;
 
     		_Y.on('domready', function () {
     			CMBSU_CSS.createPageDropEditor();
+    			CMBSU_CSS.createSliderSearch();
     		});
     	});
     	
