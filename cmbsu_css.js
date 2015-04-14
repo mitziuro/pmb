54fe513599a10fc7ad245410af0e8f072e64ca30
romsys <romsys@172.16.10.79>
2015-04-14 15:52:28 +0300

    asd

--- cmbsu_css.js
+++ cmbsu_css.js
@@ -5,10 +5,10 @@ var CMBSU_CSS = {
 	createPageDropEditor: function() {
 		//alert(_Y.one(document'#T:wc_psl36::r'));
 		//alert(_Y.one('T:wc_psl36::r'));
-		_Y.one(document.getElementById('#T:wc_psl36::r')).setStyle('right', '36px');
+		_Y.one(document.getElementById('T:wc_psl36::r')).setStyle('right', '36px');
 		
-		_Y.one(document.getElementById('#T:globNavItms')).setStyle('position', 'relative');
-		_Y.one(document.getElementById('#T:globNavItms')).setStyle('left', '-1px');
+		_Y.one(document.getElementById('T:globNavItms')).setStyle('position', 'relative');
+		_Y.one(document.getElementById('T:globNavItms')).setStyle('left', '-1px');
 		
 	},
 
