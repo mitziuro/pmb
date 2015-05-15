634807ea026afc44fc886976db67f0ada9f1fd1e
romsys <romsys@172.16.10.79>
2015-05-15 16:36:38 +0300

    asdas

--- cmbsu_css.js
+++ cmbsu_css.js
@@ -93,6 +93,7 @@ var CMBSU_CSS = {
 		
 		document.getElementById('T:pgl1').insertBefore(document.getElementById('T:wc_psl3::t'), document.getElementById('T:wc_psl1::t'));
 		document.getElementById('T:wc_psl3::t').style.top = '71px';
+		document.getElementById('T:wc_psl3::t').className += " div_menu";
 		
 		//content
 		document.getElementById('T:hm_pnc2').style.padding = '';
