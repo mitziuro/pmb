78c9df0c60c608973ca3bde7121907fc1bbacb5c
romsys <romsys@172.16.10.79>
2015-05-18 20:15:49 +0300

    werwe

--- cmbsu_alert.js
+++ cmbsu_alert.js
@@ -5,6 +5,7 @@ var CMBSU_ALERT = {
 		div.innerHTML = '<div style="float:left"><img style="height:40px;padding-top:5px;" src="https://rawgit.com/mitziuro/pmb/master/alert-sign.gif"/></div>';
 		div.innerHTML += '<div class="alert_content">Alerta</div>';
 		div.className= "alert_div";
+		div.style.position = 'absolute';
 		//document.getElementById('T:pcl1::c').insertBefore(div, document.getElementById('T:pcl1::c').firstChild);   
 		document.getElementById('T:pgl1').insertBefore(div, document.getElementById('T:pgl1').firstChild);    
 		
