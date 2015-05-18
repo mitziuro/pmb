e24eaef3c76e26226a431b7f96df49b24afbe81c
romsys <romsys@172.16.10.79>
2015-05-18 18:58:28 +0300

    asdas

--- cmbsu_alert.js
+++ cmbsu_alert.js
@@ -3,7 +3,9 @@ var CMBSU_ALERT = {
 	writeAlert: function() {
 		
 		var div = document.createElement("div");
+		
 		div.innerHTML = 'HANNA ARE Kortosh';
+		div.className= "alert_div";
 		
 		document.getElementById('T:pgl1').insertBefore(div, document.getElementById('T:pgl1').firstChild);    
 		
