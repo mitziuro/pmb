81cb49e163560806caba2bb51c617623125645a6
romsys <romsys@172.16.10.79>
2015-05-18 19:52:12 +0300

    asd

--- cmbsu_alert.js
+++ cmbsu_alert.js
@@ -2,7 +2,8 @@ var CMBSU_ALERT = {
 
 	writeAlert: function() {
 		var div = document.createElement("div");
-		div.innerHTML = '<div style="float:left"><img style="width:10px;height:10px" src="https://rawgit.com/mitziuro/pmb/master/alert-sign.jpg"/></div><div>Alerta</div>';
+		div.innerHTML = '<div style="float:left"><img style="height:40px" src="https://rawgit.com/mitziuro/pmb/master/alert-sign.jpg"/></div>';
+		div.innerHTML += '<div class="alert_content">Alerta</div>';
 		div.className= "alert_div";
 		document.getElementById('T:pcl1::c').insertBefore(div, document.getElementById('T:pcl1::c').firstChild);    
 	},
