1872eb5b4d958d5feb825dff8d772e7c4410e46b
romsys <romsys@172.16.10.79>
2015-05-18 14:57:50 +0300

    fdsfd

--- cmbsu_css.js
+++ cmbsu_css.js
@@ -116,7 +116,7 @@ var CMBSU_CSS = {
 		var div = document.createElement("div");
 		document.getElementById('T:pgl1').appendChild(div);
 		
-		document.getElementById('T:pgl2').style.minHeight= '768px';
+		document.getElementById('T:pgl2').style.minHeight= '550px';
 		
 		div.innerHTML = '<div class="footer_div"> \
 		<table summary="" class="footer_table x25t x1a" style="text-align:center;" border="0" cellpadding="0" cellspacing="0">\
