a547606c2da636e62aa0952071e312d09947697e
romsys <romsys@172.16.10.79>
2015-05-18 17:35:55 +0300

    wreer

--- cmbsu_css.js
+++ cmbsu_css.js
@@ -89,7 +89,8 @@ var CMBSU_CSS = {
 	createLayout: function() {
 		
 		//top
-		document.getElementById('T:globalContainer').style.paddingBottom = '14px'; 
+		document.getElementById('T:globalContainer').style.paddingBottom = '11px'; 
+		document.getElementById('T:globalContainer').style.overflow = 'hidden'; 
 		
 		
 		document.getElementById('T:pgl1').insertBefore(document.getElementById('T:wc_psl1::t'), document.getElementById('T:pgl1').firstChild);    
