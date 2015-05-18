b568603c6ac19290afad7f35a030a413a333ca03
romsys <romsys@172.16.10.79>
2015-05-18 19:41:20 +0300

    asd

--- cmbsu_alert.js
+++ cmbsu_alert.js
@@ -2,7 +2,7 @@ var CMBSU_ALERT = {
 
 	writeAlert: function() {
 		var div = document.createElement("div");
-		div.innerHTML = 'HANNA ARE Kortosh';
+		div.innerHTML = '<div style="float:left"><img src="https://rawgit.com/mitziuro/pmb/master/warning-sign.jpg"/></div><div>Alerta</div>';
 		div.className= "alert_div";
 		document.getElementById('T:pcl1::c').insertBefore(div, document.getElementById('T:pcl1::c').firstChild);    
 	},
