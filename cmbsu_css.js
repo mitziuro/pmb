d031771799d1f664f33236f678d4e0c7482d58a9
romsys <romsys@172.16.10.79>
2015-05-19 03:55:03 +0300

    asdasd

--- cmbsu_css.js
+++ cmbsu_css.js
@@ -164,6 +164,8 @@ var CMBSU_CSS = {
 		document.getElementById('f1').insertBefore(document.getElementById('T:pgl1'), document.getElementById('f1').firstChild);    
 		document.getElementById('f1').removeChild(document.getElementById('T:globalContainer'));
 		document.getElementById('T:pgl1').style.overflow = '';
+		document.getElementById('T:wc_psl369').style.height = '0px';
+		
 		
 	},
 	
