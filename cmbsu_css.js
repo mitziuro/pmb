52dbe24108962a5a37e15c6c606b42064bbe31ce
romsys <romsys@172.16.10.79>
2015-05-19 12:09:10 +0300

    wedwqed

--- cmbsu_css.js
+++ cmbsu_css.js
@@ -151,8 +151,7 @@ var CMBSU_CSS = {
 						<div class="footer_div_">\
 							<a href="http://www.smurd.ro">Serviciul Mobil de Urgenta, Reanimare si Descarcerare</a> \
 						</div>\
-						<br/>\
-						<br/>\
+						<div style="height:70px;">\
 						<div class="footer_div_"> \
 							<a href="http://sabif.ro">Serviciul de Ambulanta Bucuresti-Ilfov</a>\
 						</div>\
