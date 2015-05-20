311a3549511bd39a6e70ede3243286ff499fe1f3
romsys <romsys@172.16.10.79>
2015-05-20 12:53:42 +0300

    asdfsa

--- cmbsu.js
+++ cmbsu.js
@@ -25,11 +25,20 @@ var CMBSU = {
     	
    
     	CMBSU.loadScript('http://yui.yahooapis.com/3.18.1/build/yui/yui-min.js', function(){});
-    	CMBSU.loadScript('http://code.jquery.com/jquery-2.1.4.min.js', function(){});
+    	CMBSU.loadScript('http://code.jquery.com/jquery-2.1.4.min.js', function(){
+    		
+    		
+    		CMBSU.loadScript('http://sorgalla.com/jcarousel/dist/jquery.jcarousel.min.js', function(){
+    			//carrousel
+    			CMBSU.loadScript(CMBSU_URL + 'cmbsu_carrousel.js', function(){});
+    			
+    		});
+    		
+    	});
     	
-    	//carrousel
-    	CMBSU.loadScript('http://sorgalla.com/jcarousel/dist/jquery.jcarousel.min.js', function(){});
-    	CMBSU.loadScript(CMBSU_URL + 'cmbsu_carrousel.js', function(){});
+    	
+    	
+    	
     	alert(7);
     	
     	CMBSU.loadScript(CMBSU_URL + 'cmbsu_css.js', function(){});
