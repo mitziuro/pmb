a138d24dc662e271dae10a42f512dcc493105c19
romsys <romsys@172.16.10.79>
2015-05-22 17:57:07 +0300

    sfddsd

--- cmbsu_carrousel.js
+++ cmbsu_carrousel.js
@@ -2,12 +2,4 @@ var swiper = new Swiper('.swiper-container', {
     pagination: '.swiper-pagination',
     paginationClickable: true,
     autoplay: true,
-    effect: 'cube',
-    speed: 1000,
-    cube: {
-    	  slideShadows: true,
-    	  shadow: true,
-    	  shadowOffset: 20,
-    	  shadowScale: 0.94
-    	}
 });
