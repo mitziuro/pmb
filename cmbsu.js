a4b04957ecdfe4a0a5432f1afb8bca4134ac6508
romsys <romsys@172.16.10.79>
2015-05-22 16:53:36 +0300

    sfsafd

--- cmbsu.js
+++ cmbsu.js
@@ -28,7 +28,7 @@ var CMBSU = {
     	CMBSU.loadScript('http://code.jquery.com/jquery-2.1.4.min.js', function(){
     	
     		//carrousel
-    		CMBSU.loadScript('http://bxslider.com/lib/jquery.bxslider.js', function(){
+    		CMBSU.loadScript(CMBSU_URL + swiper.min.js, function(){
     			
     			CMBSU.loadScript(CMBSU_URL + 'cmbsu_carrousel.js', function(){});
     			
--- cmbsu_carrousel.js
+++ cmbsu_carrousel.js
@@ -1,3 +1,4 @@
-$(document).ready(function(){
-  $('.bxslider').bxSlider();
-});
+var swiper = new Swiper('.anno_div', {
+        pagination: '.anno_div_pagination',
+        paginationClickable: true
+    });
