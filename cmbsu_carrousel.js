de2c1e53978043c478ba4ea5b31afe8c3341fc18
romsys <romsys@172.16.10.79>
2015-05-22 17:51:15 +0300

    asdasd

--- cmbsu_carrousel.js
+++ cmbsu_carrousel.js
@@ -1,4 +1,13 @@
 var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        paginationClickable: true
-    });
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    autoplay: true,
+    effect: 'cube',
+    
+    cube: {
+    	  slideShadows: true,
+    	  shadow: true,
+    	  shadowOffset: 20,
+    	  shadowScale: 0.94
+    	}
+});
