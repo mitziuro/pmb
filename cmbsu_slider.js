d030387c7c29366403467e073cd1044e8b146246
Mihai Plavichianu <mitziuro@users.noreply.github.com>
2015-05-25 14:18:24 +0300

    asdas

--- cmbsu_slider.js
+++ cmbsu_slider.js
@@ -1,15 +1,12 @@
 alert(3);
 
 
-$(function(){
-        $('#T:portlet9076752699').tabSlideOut({
-            tabHandle: '.handle',                     //class of the element that will become your tab
-            tabLocation: 'left',                      //side of screen where tab lives, top, right, bottom, or left
-            speed: 300,                               //speed of animation
-            action: 'click',                          //options: 'click' or 'hover', action to trigger animation
-            topPos: '200px',                          //position from the top/ use if tabLocation is left or right
-            leftPos: '20px',                          //position from left/ use if tabLocation is bottom or top
-            fixedPosition: false                      //options: true makes it stick(fixed position) on scroll
-        });
-
-    });
+$('#T:portlet9076752699').tabSlideOut({
+    tabHandle: '.handle',                     //class of the element that will become your tab
+    tabLocation: 'left',                      //side of screen where tab lives, top, right, bottom, or left
+    speed: 300,                               //speed of animation
+    action: 'click',                          //options: 'click' or 'hover', action to trigger animation
+    topPos: '200px',                          //position from the top/ use if tabLocation is left or right
+    leftPos: '20px',                          //position from left/ use if tabLocation is bottom or top
+    fixedPosition: false                      //options: true makes it stick(fixed position) on scroll
+});
