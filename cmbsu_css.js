e76a3a46c270fce0a6a4251953088ed6b262bc24
Mihai Plavichianu <mitziuro@users.noreply.github.com>
2015-05-25 18:11:51 +0300

    erew

--- cmbsu_css.js
+++ cmbsu_css.js
@@ -272,7 +272,7 @@ var CMBSU_CSS = {
 			alert(6);
 			
 			document.getElementById('T:oc_1718626991region1:tspgl2').firstChild.onClick = function() {
-				alert(65);
+				alert(document.getElementById('T:oc_1718626991region1:tspgl2').firstChild.tagName);
 				$(document.getElementById('T:oc_1718626991region1:tspgl2').firstChild).effect('slide', {}, 1000, function() {});
 			};
 			
