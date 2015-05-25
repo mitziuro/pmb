0a39c42e82e3c21111ebd5c5be75e3a8bd2d4801
Mihai Plavichianu <mitziuro@users.noreply.github.com>
2015-05-25 15:35:32 +0300

    wewerwe

--- cmbsu_css.js
+++ cmbsu_css.js
@@ -263,7 +263,7 @@ var CMBSU_CSS = {
 	
 	quizModify: function() {
 		
-		document.getElementById('T:portlet9076752699').className += ' quizTd';
+		document.getElementById('T:oc_1718626991region1:srpsl1::c).className += ' quizTd';
 		
 		//var tds = document.getElementById('T:pgl8241733443').firstChild.firstChild.childNodes;
 		//tds[1].className += 'quizTd';
