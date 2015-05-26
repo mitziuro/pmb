72f773452cccc0757dee65d92bf3b0d6556713d3
Mihai Plavichianu <mitziuro@users.noreply.github.com>
2015-05-26 12:00:42 +0300

    sdfdf

--- cmbsu_carrousel.js
+++ cmbsu_carrousel.js
@@ -3,5 +3,16 @@ var swiper = new Swiper('.swiper-container', {
     paginationClickable: true,
     autoplay: true,
     autoplayDisableOnInteraction: false,
-    speed: 2000
+    speed: 2000,
+    slideClass: 'swiper-slide', 
+    slideActiveClass: 'swiper-slide-active',
+    slideVisibleClass: 'swiper-slide-visible',
+    slideDuplicateClass: 'swiper-slide-duplicate',
+    slideNextClass: 'swiper-slide-next',
+    slidePrevClass: 'swiper-slide-prev',
+    wrapperClass: 'swiper-wrapper',
+    bulletClass: 'swiper-pagination-bullet',
+    bulletActiveClass: 'swiper-pagination-bullet-active',
+    paginationHiddenClass: 'swiper-pagination-hidden',
+    buttonDisabledClass: 'swiper-button-disabled'
 });
