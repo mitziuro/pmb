8e335ee07e0c9b61fb1338f320f4fa9e9fd8d5f0
Mihai Plavichianu <mitziuro@users.noreply.github.com>
2015-05-28 17:26:24 +0300

    zxcz

--- cmbsu_css.js
+++ cmbsu_css.js
@@ -370,9 +370,6 @@ var CMBSU_CSS = {
 		var div = document.createElement("div");
 		document.getElementById('T:pgl1').appendChild(div);
 		div.id = 'user_profile_';
-		div.style.position = 'fixed';
-		div.style.height = '300px';
-		div.style.width = '300px';
 		div.innerHTML = 'hanna are mere';
 	},
 	
