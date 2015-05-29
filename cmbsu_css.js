58edc858beb740ffdb297c7ddecab7cd7758ec33
Mihai Plavichianu <mitziuro@users.noreply.github.com>
2015-05-29 18:22:25 +0300

    dsadsadsa

--- cmbsu_css.js
+++ cmbsu_css.js
@@ -218,9 +218,7 @@ var CMBSU_CSS = {
 		img.className = 'loadingImg';
 		div.appendChild(img);
 		alert(pmbDiv.childNodes[0]);
-		pmbDiv.childNodes[0].onLoad = function() {
-			alert(55);
-		};
+		pmbDiv.childNodes[0].onLoad = 'alert(66);';
 		pmbDiv.insertBefore(div, pmbDiv.childNodes[0]);
 		
 	},
