b2915a76fa34c4e283217b27ec58f01257822ebd
Mihai Plavichianu <mitziuro@users.noreply.github.com>
2015-06-02 13:50:00 +0300

    dfsf

--- cmbsu_css.js
+++ cmbsu_css.js
@@ -224,6 +224,7 @@ var CMBSU_CSS = {
 	},
 	
 	hideLoader: function(iframe) {
+		alert(document.getElementById(iframe.id));
 		document.getElementById(iframe.id).visibility = 'hidden';
 		iframe.style.visibility = 'visible';
 	},
