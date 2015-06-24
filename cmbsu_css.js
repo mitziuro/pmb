a347fd7940e2422f67b0b9b21f3a4677d78896d8
Mihai Plavichianu <mitziuro@users.noreply.github.com>
2015-06-24 16:30:48 +0300

    cxzxzccx

--- cmbsu_css.js
+++ cmbsu_css.js
@@ -572,7 +572,15 @@ var CMBSU_CSS = {
 		}
 	
 	
+		if(document.getElementById('T:theTaskFlow:dsr1:ssresfph1j_id_9') != null) {
+			document.getElementById('T:theTaskFlow:dsr1:ssresfph1j_id_9').childNodes[0].style.borderBottom = '0px !important';	
+		}
+		
+		if(document.getElementById('T:theTaskFlow:dsr1:pb1j_id_9') != null) {
+			document.getElementById('T:theTaskFlow:dsr1:pb1j_id_9').style.border = '0px !important';	
+		}
 
+		
 		i=0;
 		
 		while(true) {
